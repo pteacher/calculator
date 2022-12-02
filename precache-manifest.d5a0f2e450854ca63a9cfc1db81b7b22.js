55fe35847aa83814a4a0f15367b6e8d296a9bdc3
pteacher <subfear@mail.ru>
2022-12-02 10:17:29 +0600

    Updates

---
+++ precache-manifest.d5a0f2e450854ca63a9cfc1db81b7b22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9a33055c7ec9adb0d0412441483abf5",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calculator/static/css/2.f87fbcaa.chunk.css"
+  },
+  {
+    "revision": "a46a32d56b943b42c2ee",
+    "url": "/calculator/static/css/main.c5ba3a1e.chunk.css"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calculator/static/js/2.9067ee69.chunk.js"
+  },
+  {
+    "revision": "a46a32d56b943b42c2ee",
+    "url": "/calculator/static/js/main.e5208534.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  }
+]);
